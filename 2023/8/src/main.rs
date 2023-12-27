ab94c2f1d16b1b45d099e1446a80100a4a158bef
Petróczi Zoltán <github@petroczi.hu>
2023-12-28 00:56:00 +0100

    8.2

--- 2023/8/src/main.rs
+++ 2023/8/src/main.rs
@@ -1,5 +1,7 @@
 use std::{env, fs::read_to_string, collections::HashMap};
 
+use num::integer::lcm;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -14,13 +16,19 @@ fn main() {
     let instructions = lines.first().unwrap();
 
     let mut sum1: i64 = 0;
-    let mut sum2: i64 = 0;
+    let mut sum2: i64 = 1;
 
     let mut location = "AAA";
-    let mut locations= HashMap::new();
+    let mut locations = HashMap::new();
+
+    let mut locations_2 = Vec::new();
 
     for line in lines.iter().skip(2) {
         let [loc, _, l, r] = line.split(' ').collect::<Vec<&str>>()[..] else { panic!() };
+
+        if loc.ends_with("A") {
+            locations_2.push(loc);
+        }
 
         let left = l.get(1..=3).unwrap();
         let right = r.get(0..=2).unwrap();
@@ -33,17 +41,40 @@ fn main() {
     for instruction in instructions_iter {
         sum1 += 1;
 
-        match instruction {
-            'L' => location = &locations.get(location).unwrap().0,
-            'R' => location = &locations.get(location).unwrap().1,
-            _ => panic!(),
-        }
+        location = new_location(&locations, location, instruction);
 
         if location == "ZZZ" {
             break;
         }
     }
 
+
+
+    for location_2 in locations_2 {
+        let mut location = location_2;
+
+        let instructions_iter = instructions.chars().cycle();
+        let mut count = 0;
+
+        for instruction in instructions_iter {
+            count += 1;
+            location = new_location(&locations, location, instruction);
+            if location.ends_with("Z") {
+                break;
+            }
+        }
+
+        sum2 = lcm(sum2, count); 
+    }
+
     println!("1: {}", sum1);
     println!("2: {}", sum2);
+}
+
+fn new_location<'a>(locations: &HashMap<&'a str, (&'a str, &'a str)>, location: &'a str, instruction: char) -> &'a str {
+        match instruction {
+            'L' => return &locations.get(location).unwrap().0,
+            'R' => return &locations.get(location).unwrap().1,
+            _ => panic!(),
+        }
 }
